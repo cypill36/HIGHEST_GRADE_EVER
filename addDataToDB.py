212474bb5c02c092c48ea6c68570924995fb74a4
cypill36 <cyril_pillai1@baylor.edu>
2023-04-17 16:39:11 -0500

    initial python stuff

--- addDataToDB.py
+++ addDataToDB.py
@@ -1,5 +1,33 @@
 import pymysql
 import csi3335 as cfg
 
-con = pymysql.connect(host=cfg.mysql['location'], user=cfg.mysql['user'], password=cfg.mysql['password'], database=cfg.mysql['database'])
+con = pymysql.connect(host     = cfg.mysql['location'], 
+                      user     = cfg.mysql['user'], 
+                      password = cfg.mysql['password'], 
+                      database = cfg.mysql['database'])
+
+try:
+    cur = con.cursor()
+    sql = '''SELECT * FROM batting 
+             '''
+    cur.execute(sql)
+    
+    column_names = [i[0] for i in cur.description]
+
+    for d in cur.description:
+        print(d)
+
+    #print(column_names)
+
+except Exception:
+    con.rollback()
+    print("Database Exception.")
+    raise
+else:
+    con.commit()
+finally:
+    con.close()
+
+
+
 
