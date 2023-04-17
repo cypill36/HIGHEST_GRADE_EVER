dde733e1caef12b8b33efbdd8ce28650e0b36ef7
alexkubicek <127884841+alexkubicek@users.noreply.github.com>
2023-04-17 16:06:32 -0500

    added to python file for adding data to database

--- addDataToDB.py
+++ addDataToDB.py
@@ -1,4 +1,5 @@
 import pymysql
 import csi3335 as cfg
 
-con = pymysql.connect(host=cfg.mysql['location'], user=cfg.mysql['user'], password=cfg.mysql['password'], dv=cfg.mysql['database'])
+con = pymysql.connect(host=cfg.mysql['location'], user=cfg.mysql['user'], password=cfg.mysql['password'], database=cfg.mysql['database'])
+
