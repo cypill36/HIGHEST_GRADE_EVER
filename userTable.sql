722089106ea0dbef6332cd2ec46547b2b4c90ded
kaylynnbeard <kaylynn_beard1@baylor.edu>
2023-04-19 15:56:36 -0500

    Creates user table and logs table

--- userTable.sql
+++
@@ -1,9 +0,0 @@
-
--- stores information about user
-CREATE TABLE users (
-	userID int NOT NULL AUTO_INCREMENT,
-	username varchar(255) NOT NULL,
-	password varchar(255) NOT NULL,
-	isAdmin varchar(1) DEFAULT 'N',
-	PRIMARY KEY(userID)
-)
