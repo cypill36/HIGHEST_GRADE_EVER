30d2fe73d934f3599879e11c30f02b4f13358369
kaylynnbeard <kaylynn_beard1@baylor.edu>
2023-04-23 18:07:47 -0500

    Rename ID attributes to unique names

---
+++ renameIDs.py
@@ -0,0 +1,41 @@
+import pymysql
+import csi3335sp2023 as cfg
+
+con = pymysql.connect(host=cfg.mysql['location'],
+                      user=cfg.mysql['user'],
+                      password=cfg.mysql['password'],
+                      database=cfg.mysql['database'])
+
+tables = []
+
+try:
+    cur = con.cursor()
+    sql = "SHOW tables"
+    cur.execute(sql)
+    results = cur.fetchall()
+    for t in results:
+        tables.append(t[0])
+
+    # iterate over tables
+    for t in tables:
+        attributes = []
+        sql = "DESCRIBE " + t
+        cur.execute(sql)
+        results = cur.fetchall()
+        firstRow = results[0]
+
+        # if table has ID attribute
+        if firstRow[0] == 'ID':
+            uniqueID = t + "ID"
+            # rename ID column to tableID
+            sql = "ALTER TABLE " + t + " CHANGE COLUMN `ID` `" + uniqueID + "` int(12);"
+            cur.execute(sql)
+
+except Exception:
+    con.rollback()
+    print("Database Exception.")
+    raise
+else:
+    con.commit()
+finally:
+    con.close()
