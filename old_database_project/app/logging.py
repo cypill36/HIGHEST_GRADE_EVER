ce4a316cbf5265140d2eda44a018326a8ccbc094
kaylynnbeard <kaylynn_beard1@baylor.edu>
2023-05-03 16:05:38 -0500

    Removed old files

--- old_database_project/app/logging.py
+++
@@ -1,8 +0,0 @@
-import time
-from .models import Logs, db
-
-def log_standing_selection(username, yearID, team_name):
-    new_log_entry = Logs(username = username, yearID = yearID, team_name = team_name, timestamp = int(time.time()))
-
-    db.session.add(new_log_entry)
-    db.session.commit()
